d0a1f86f59b2a0afb3f275bf8b14d1740bba6694
VestingDart <sasuke.uchiha2010@yahoo.com>
2025-03-19 21:59:32 +0100

    Added number guessing system

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
+
+    static Integer myNumber = ThreadLocalRandom.current().nextInt(0, 100 + 1);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        guess(41);
+        guess(42);
     }
+
+    public static void guess(Integer number)  {
+        if(number == myNumber) {
+            System.out.println("Guessed right!");
+        } else {
+            System.out.println("Guessed false!");
+        }
+    }
+
 }
